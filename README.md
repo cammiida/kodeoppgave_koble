3ad5b31ec6a0a2c51e6c6b8bc9250bbf62037614
Camilla <cammiida@gmail.com>
2019-03-22 08:50:38 +0100

    Initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# kodeoppgave_koble
---
+++ db/db.js
@@ -0,0 +1,9 @@
+const urls = [
+  {
+    id: 1,
+    shortUrl: "",
+    longUrl: "https://www.koble.co/companies/koble/postings/0G8fVZ/"
+  }
+];
+
+export default urls;
